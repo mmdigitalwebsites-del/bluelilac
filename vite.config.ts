28a1a813c50ad037c5aff311884ed61a113eb7a2
mmdigital <mmdigitalwebsites@gmail.com>
2026-06-24 16:51:14 +0300

    switch to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: { server: { entry: "server" } },
-  nitro: { preset: "netlify" }, // add this
+  nitro: { preset: "vercel" }, // add this
 });
